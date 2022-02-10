8312927eaf3feec109e2e0f708162731672ae779
Arjun AK <arjun.aak@protonmail.com>
2022-02-10 22:12:22 +0530

    DQN

--- Env.py
+++ Env.py
@@ -33,10 +33,33 @@ class CabDriver():
 
 
     # Use this function if you are using architecture-2
-    def state_encod_arch2(self, state, action):
-        """convert the (state-action) into a vector so that it can be fed to the NN. This method converts a given state-action pair into a vector format. Hint: The vector is of size m + t + d + m + m."""
+    # def state_encod_arch2(self, state, action):
+    #     """convert the (state-action) into a vector so that it can be fed to the NN. This method converts a given state-action pair into a vector format. Hint: The vector is of size m + t + d + m + m."""
+    #
+    #     vector_size = m + t + d + m + m
+    #     state_encod = [0]*vector_size
+    #
+    #     location_index = state[0] - 1
+    #     state_encod[location_index] = 1
+    #
+    #     hour_index = m + state[1]
+    #     state_encod[hour_index] = 1
+    #
+    #     day_index = m + t + state[2]
+    #     state_encod[day_index] = 1
+    #
+    #     pick_up_index = m + t + d + action[0] - 1
+    #     state_encod[pick_up_index] = 1
+    #
+    #     drop_index = m + t + d + m + action[1] - 1
+    #     state_encod[drop_index] = 1
+    #
+    #     return state_encod
 
-        vector_size = m + t + d + m + m
+    def state_encod_arch2(self, state):
+        """convert the state into a vector so that it can be fed to the NN. This method converts a given state-action pair into a vector format. Hint: The vector is of size m + t + d"""
+
+        vector_size = m + t + d
         state_encod = [0]*vector_size
 
         location_index = state[0] - 1
@@ -48,14 +71,7 @@ class CabDriver():
         day_index = m + t + state[2]
         state_encod[day_index] = 1
 
-        pick_up_index = m + t + d + action[0] - 1
-        state_encod[pick_up_index] = 1
-
-        drop_index = m + t + d + m + action[1] - 1
-        state_encod[drop_index] = 1
-
         return state_encod
-
 
     ## Getting number of requests
     def requests(self, state):
@@ -83,26 +99,25 @@ class CabDriver():
 
         return possible_actions_index,actions
 
-    def calc_new_time_and_day(time_of_day, day_of_week, travel_time):
+    def calc_new_time_and_day(self, time_of_day, day_of_week, travel_time):
         """Return new time of day and day of the week after adding the travel time."""
         time_of_day = (time_of_day + travel_time) % (t-1)
         day_of_week = (day_of_week + (travel_time // (t-1)))%(d-1)
 
         return time_of_day, day_of_week
 
-    def calc_travel_time(current_loc, start_loc, end_loc, time_of_day, day_of_week, Time_matrix):
-        """Returns t1: time taken from current location to start locations
-                   t2: time taken from start location to end location"""
+    def calc_travel_time(self, current_loc, start_loc, end_loc, time_of_day, day_of_week, Time_matrix):
+        """Returns t1: time taken from current location to start locations; t2: time taken from start location to end location"""
         if start_loc == 0 and end_loc == 0:
             return 0, 1 #ride not accepeted by driver so increment time by 1 hour
 
         if current_loc == start_loc:
             t1 = 0
         else:
-            t1 = int(Time_matrix[current_loc-1][start_location-1][time_of_day][day_of_week])
-            time_of_day, day_of_week = calc_new_time_and_day(time_of_day, day_of_week, t1)
+            t1 = int(Time_matrix[current_loc-1][start_loc-1][time_of_day][day_of_week])
+            time_of_day, day_of_week = self.calc_new_time_and_day(time_of_day, day_of_week, t1)
 
-        t2 = int(Time_matrix[start_location-1][end_location-1][time_of_day][day_of_week])
+        t2 = int(Time_matrix[start_loc-1][end_loc-1][time_of_day][day_of_week])
 
         return t1, t2
 
@@ -114,7 +129,7 @@ class CabDriver():
         start_loc = action[0]
         end_loc = action[1]
 
-        t1, t2 = calc_travel_time(current_loc, start_loc, end_loc, time_of_day, day_of_week, Time_matrix)
+        t1, t2 = self.calc_travel_time(current_loc, start_loc, end_loc, time_of_day, day_of_week, Time_matrix)
 
         if start_loc != 0 or end_loc != 0: #action not (0,0)
             reward = R * t2 - C * (t1 + t2)
@@ -123,6 +138,17 @@ class CabDriver():
 
         return reward
 
+    def calc_total_time(self, state, action, Time_matrix):
+        """Return total time of the trip"""
+        current_loc = state[0]
+        time_of_day = state[1]
+        day_of_week = state[2]
+        start_loc = action[0]
+        end_loc = action[1]
+
+        t1, t2 = self.calc_travel_time(current_loc, start_loc, end_loc, time_of_day, day_of_week, Time_matrix)
+
+        return t1+t2
 
     def next_state_func(self, state, action, Time_matrix):
         """Takes state and action as input and returns next state"""
@@ -132,18 +158,17 @@ class CabDriver():
         start_loc = action[0]
         end_loc = action[1]
 
-        t1, t2 = calc_travel_time(current_loc, start_loc, end_loc, time_of_day, day_of_week, Time_matrix)
+        t1, t2 = self.calc_travel_time(current_loc, start_loc, end_loc, time_of_day, day_of_week, Time_matrix)
         total_travel_time = t1 + t2
 
+        new_loc = current_loc
         if start_loc != 0 or end_loc != 0:
             new_loc = end_loc
-        new_time_of_day, new_day_of_week = calc_new_time_and_day(time_of_day, day_of_week, total_travel_time)
+        new_time_of_day, new_day_of_week = self.calc_new_time_and_day(time_of_day, day_of_week, total_travel_time)
 
         next_state = (new_loc, new_time_of_day, new_day_of_week)
 
         return next_state
-
-
 
 
     def reset(self):
